dfa0353ed1fa4b1a46213b5998c667244323fb41
Helena Rasche <hxr@hx42.org>
2021-06-18 10:57:32 +0200

    cleaner works ok

--- clean.sh
+++ clean.sh
@@ -1,1 +1,3 @@
-sed -i 's/inkscape:label="[^"]*"//g;s/inkscape:connector-curvature="0"//g;s/id="path[^"]*"//g' $1
+f=$(mktemp)
+sed 's/inkscape:label="[^"]*"//g;s/inkscape:connector-curvature="0"//g;s/id="path[^"]*"//g;s/\t//g' < "$1" | tr -d '\n' > "$f"
+mv "$f" "$1"
