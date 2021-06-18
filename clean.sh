88b564e2e2b6f59adacdb57e118830534004cb5d
Helena Rasche <hxr@hx42.org>
2021-06-18 11:23:33 +0200

    ok, done

--- clean.sh
+++ clean.sh
@@ -1,3 +1,7 @@
 f=$(mktemp)
-sed 's/inkscape:label="[^"]*"//g;s/inkscape:connector-curvature="0"//g;s/id="path[^"]*"//g;s/\t//g' < "$1" | tr -d '\n' > "$f"
+sed 's/inkscape:label="[^"]*"//g;s/inkscape:connector-curvature="0"//g;s/id="path[^"]*"//g;s/\t//g' < "$1" | \
+	sed "s|font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-size:16px;font-family:'Exo 2';-inkscape-font-specification:'Exo 2, ';||g" | \
+	sed "s|font-style:normal;font-variant:normal;font-weight:normal;font-stretch:normal;font-family:'Exo 2';-inkscape-font-specification:'Exo 2, ';||g" | \
+	sed "s|fill-opacity:1;||g;s|;fill-opacity:1||g;s|fill-opacity:1||g;s|stroke-opacity:1;||g;s|;stroke-opacity:1||g;s|stroke-opacity:1||g;s|stop-opacity:1;||g;s|;stop-opacity:1||g;s|stop-opacity:1||g;s|opacity:1;||g;s|;opacity:1||g;s|opacity:1||g;" | \
+	tr -d '\n' > "$f"
 mv "$f" "$1"
